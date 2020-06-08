877f554c13f54e39e0612503dccbbaa971108148
iamazy <1448588084@qq.com>
2020-06-09 01:24:07 +0800

    Update protocol.rs

--- src/protocol.rs
+++ src/protocol.rs
@@ -11,8 +11,8 @@ impl Protocol {
 
     pub(crate) fn new(name: &str, level: u8) -> Result<Protocol, Error> {
         match (name, level) {
-            /// I dont know the protocol level of `MQTT5`, set level to 5 temporarily
-            ("MQTT5", 5) => Ok(Protocol::MQTT5),
+            /// Protocol Level: https://docs.oasis-open.org/mqtt/mqtt/v5.0/os/mqtt-v5.0-os.html#_Toc3901033
+            ("MQTT5", 5u8) => Ok(Protocol::MQTT5),
             _ => Err(Error::InvalidProtocol(name.into(), level))
         }
     }
@@ -20,7 +20,10 @@ impl Protocol {
     pub(crate) fn to_buf(&self, buf: &mut impl BufMut) -> Result<usize, Error> {
         match self {
             Protocol::MQTT5 => {
-                let slice = &[0u8, 4, 'M' as u8, 'Q' as u8, 'T' as u8, 'T' as u8, 4];
+                /// offset: 0
+                /// length: 4
+                /// body: MQTT
+                let slice = &[0u8, 4u8, 'M' as u8, 'Q' as u8, 'T' as u8, 'T' as u8];
                 buf.put_slice(slice);
                 Ok(slice.len())
             }
