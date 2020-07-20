08edb6ebed796fc981187fb66841a5eb1e9ec432
iamazy <1448588084@qq.com>
2020-07-20 23:26:07 +0800

    Update packet.rs

--- mqtt-codec/src/packet.rs
+++ mqtt-codec/src/packet.rs
@@ -144,87 +144,88 @@ impl Packet {
 
     pub fn parse(buf: &mut BytesMut) -> Result<Packet, Error> {
         if buf.remaining() <= 1 {
-            return Err(Error::MalformedFixedHeader);
+            return Err(Error::Incomplete);
         }
         let fixed_header = FixedHeader::from_buf(buf)
             .expect("Failed to parse Fixed Header");
-        return match fixed_header.packet_type {
+        let packet =  match fixed_header.packet_type {
             PacketType::CONNECT => {
                 let connect = Connect::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse Connect Packet");
-                Ok(Packet::Connect(connect))
+                Packet::Connect(connect)
             }
             PacketType::CONNACK => {
                 let connack = ConnAck::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse ConnAck Packet");
-                Ok(Packet::ConnAck(connack))
+                Packet::ConnAck(connack)
             }
             PacketType::PUBLISH => {
                 let publish = Publish::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse Publish Packet");
-                Ok(Packet::Publish(publish))
+                Packet::Publish(publish)
             }
             PacketType::PUBACK => {
                 let puback = PubAck::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PubAck Packet");
-                Ok(Packet::PubAck(puback))
+                Packet::PubAck(puback)
             }
             PacketType::PUBREC => {
                 let pubrec = PubRec::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PubRec Packet");
-                Ok(Packet::PubRec(pubrec))
+                Packet::PubRec(pubrec)
             }
             PacketType::PUBREL => {
                 let pubrel = PubRel::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PubRel Packet");
-                Ok(Packet::PubRel(pubrel))
+                Packet::PubRel(pubrel)
             }
             PacketType::PUBCOMP => {
                 let pubcomp = PubComp::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PubComp Packet");
-                Ok(Packet::PubComp(pubcomp))
+                Packet::PubComp(pubcomp)
             }
             PacketType::SUBSCRIBE => {
                 let subscribe = Subscribe::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse Subscribe Packet");
-                Ok(Packet::Subscribe(subscribe))
+                Packet::Subscribe(subscribe)
             }
             PacketType::SUBACK => {
                 let suback = SubAck::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse SubAck Packet");
-                Ok(Packet::SubAck(suback))
+                Packet::SubAck(suback)
             }
             PacketType::UNSUBSCRIBE => {
                 let unsubscribe = UnSubscribe::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse UnSubscribe Packet");
-                Ok(Packet::UnSubscribe(unsubscribe))
+                Packet::UnSubscribe(unsubscribe)
             }
             PacketType::UNSUBACK => {
                 let unsuback = UnSubAck::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse UnSubAck Packet");
-                Ok(Packet::UnSubAck(unsuback))
+                Packet::UnSubAck(unsuback)
             }
             PacketType::PINGREQ => {
                 let pingreq = PingReq::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PingReq Packet");
-                Ok(Packet::PingReq(pingreq))
+                Packet::PingReq(pingreq)
             }
             PacketType::PINGRESP => {
                 let pingresp = PingResp::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse PingResp Packet");
-                Ok(Packet::PingResp(pingresp))
+                Packet::PingResp(pingresp)
             }
             PacketType::DISCONNECT => {
                 let disconnect = Disconnect::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse Disconnect Packet");
-                Ok(Packet::Disconnect(disconnect))
+                Packet::Disconnect(disconnect)
             }
             PacketType::AUTH => {
                 let auth = Auth::from_buf_extra(buf, fixed_header)
                     .expect("Failed to parse Auth Packet");
-                Ok(Packet::Auth(auth))
+                Packet::Auth(auth)
             }
-        }
+        };
+        Ok(packet)
     }
 }
 
