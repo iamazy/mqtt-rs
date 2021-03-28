74298cb107e48152efb7380ba1ccb41e01362d7d
iamazy <1448588084@qq.com>
2021-03-29 07:46:16 +0800

    Update lib.rs

--- nom-mqtt/src/lib.rs
+++ nom-mqtt/src/lib.rs
@@ -1,2 +1,12 @@
+#[macro_use]
+extern crate nom;
+
+use nom::error::{ErrorKind, VerboseError};
+use nom::Err as NomErr;
+
+type IResult<I, O, E = (I, ErrorKind)> = Result<(I, O), NomErr<E>>;
+type Res<T, U> = IResult<T, U, VerboseError<T>>;
+
 #[cfg(test)]
 mod tests;
+mod bytes;
