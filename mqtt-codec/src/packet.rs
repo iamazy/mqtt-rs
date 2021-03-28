a59a847750e0ea193e209e692fe14b921124bca8
iamazy <1448588084@qq.com>
2021-03-29 07:45:52 +0800

    add From trait Implement for Qos, PacketType

--- mqtt-codec/src/packet.rs
+++ mqtt-codec/src/packet.rs
@@ -80,6 +80,29 @@ pub enum PacketType {
     AUTH = 15,
 }
 
+impl From<u8> for PacketType {
+    fn from(byte: u8) -> Self {
+        match byte {
+            1 => PacketType::CONNECT,
+            2 => PacketType::CONNACK,
+            3 => PacketType::PUBLISH,
+            4 => PacketType::PUBACK,
+            5 => PacketType::PUBREC,
+            6 => PacketType::PUBREL,
+            7 => PacketType::PUBCOMP,
+            8 => PacketType::SUBSCRIBE,
+            9 => PacketType::SUBACK,
+            10 => PacketType::UNSUBSCRIBE,
+            11 => PacketType::UNSUBACK,
+            12 => PacketType::PINGREQ,
+            13 => PacketType::PINGRESP,
+            14 => PacketType::DISCONNECT,
+            15 => PacketType::AUTH,
+            _ => unimplemented!("no other packet type supported"),
+        }
+    }
+}
+
 impl Default for PacketType {
     fn default() -> Self {
         PacketType::DISCONNECT
